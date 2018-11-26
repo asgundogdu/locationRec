9cbf33439b6d22315549d8223914018e5e5cc348
Salih Gundogdu <salihgundogdu@Salihs-MacBook-Pro.local>
2018-11-26 00:57:33 -0500

    kfoldCV

--- CF/collaborative_filtering.py
+++ CF/collaborative_filtering.py
@@ -6,10 +6,13 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 import tqdm
-import math
+import math, os
 from sklearn.metrics import mean_squared_error
 from scipy.sparse.csgraph import minimum_spanning_tree as mst_nsim
 from scipy.sparse.linalg import svds
+from scipy.sparse import csr_matrix
+from scipy import sparse 
+import implicit
 
 from .data import preprocess_binary
 
@@ -72,13 +75,114 @@ def compute_precision_recall_N(PR, valid, N):
     return tp,fp,fn
 
 
-def svd_model(user_item_df, latent_dimension):
+def svd_model(user_item_df, latent_dimension, N):
     Checkins_demeaned = user_item_df.values/np.mean(user_item_df.values)
     U, sigma, Vt = svds(Checkins_demeaned, latent_dimension)
     sigma = np.diag(sigma)
     all_user_predicted_checkins = np.dot(np.dot(U, sigma), Vt) + np.mean(user_item_df.values)
     preds = pd.DataFrame(all_user_predicted_checkins, columns = user_item_df.columns, index=user_item_df.index)
     return preds
+
+
+def implicit_model(user_item_df, train, validate, latent_dimension, N, preproccesing):
+    if preproccesing==2:
+        validate = pd.DataFrame({'count' : validate.groupby(['user_nickname','town'])['town'].apply(len)}).reset_index()
+        validate['count'] = [1]*validate.shape[0]
+
+        train = pd.DataFrame({'count' : train.groupby(['user_nickname','town'])['town'].apply(len)}).reset_index()
+        train['count'] = [1]*train.shape[0]
+    elif preproccesing==1:
+        validate = pd.DataFrame({'count' : validate.groupby(['user_nickname','town'])['town'].apply(len)}).reset_index()
+        train = pd.DataFrame({'count' : train.groupby(['user_nickname','town'])['town'].apply(len)}).reset_index()
+
+    # initialize a model
+    model = implicit.als.AlternatingLeastSquares(factors=latent_dimension)
+    # train the model on a sparse matrix of item/user/confidence weights
+    #model.fit(csr_matrix(user_item_df.values.T))
+    # recommend items for a user
+    user_items = csr_matrix(user_item_df).T.tocsr()
+    # get top N recommendations
+    user_dict = dict(zip(list(range(len(user_item_df.index.tolist()))), user_item_df.index.tolist()))
+    user_dict_inverse = dict(zip(user_item_df.index.tolist(), list(range(len(user_item_df.index.tolist())))))
+    town_dict = dict(zip(list(range(len(user_item_df.columns.tolist()))), user_item_df.columns.tolist()))
+    town_dict_inverse = dict(zip(user_item_df.columns.tolist(), list(range(len(user_item_df.columns.tolist())))))
+    
+    # recommend items for a user
+    user_items = csr_matrix(user_item_df).T.tocsr()
+    item_users = user_items.T
+
+    # train the model on a sparse matrix of item/user/confidence weights
+    print('Tranining model...')
+    model.fit(user_items, show_progress=True)
+
+    print('Computing RMSE for training set')
+    rmse_train = rmse_implicit(user_dict, town_dict_inverse, model, user_item_df, train, item_users)
+    print('Computing RMSE for validation set')
+    rmse_valid = rmse_implicit(user_dict, town_dict_inverse, model, user_item_df, validate, item_users)
+
+    print("Calculating precision, recall on training set")
+    precisionN_train, recallN_train = prec_recall_implicit(user_dict, town_dict_inverse, town_dict, model, train, item_users, N)
+    print("Calculating precision, recall on validation set")
+    precisionN_val, recallN_val = prec_recall_implicit(user_dict, town_dict_inverse, town_dict, model, validate, item_users, N)
+    # tp, fp, fn = 0., 0., 0.
+    # for userid in tqdm.tqdm(list(user_dict.keys())[:3000], total=len(list(user_dict.keys())[:3000]),position=0):
+    #     recs = model.recommend(userid, item_users, N=N)
+    #     gt = validate[validate.user_nickname==user_dict[userid]].town.unique().tolist()
+    #     for enum, recomendation in enumerate(recs):
+    #         if enum == len(gt): break  
+    #         if town_dict[recomendation[0]] in gt:
+    #             tp += 1.
+    #         elif town_dict[recomendation[0]] not in gt:
+    #             fp += 1.
+
+    #     for real in gt:
+    #         if town_dict_inverse[real] not in [r[0] for r in recs]:
+    #             fn += 1.
+    # print('tp:{}, fp:{}, fn:{}'.format(tp,fp,fn))
+    # precisionN = tp/(tp+fp)
+    # recallN = tp/(tp+fn)
+
+    return model, precisionN_train, recallN_train, precisionN_val, recallN_val, rmse_train, rmse_valid
+
+
+def prec_recall_implicit(user_dict, town_dict_inverse, town_dict, model, validate, item_users, N):
+    tp, fp, fn = 0., 0., 0.
+    for userid in tqdm.tqdm(list(user_dict.keys())[:3000], total=len(list(user_dict.keys())[:3000]),position=0):
+        recs = model.recommend(userid, item_users, N=N)
+        gt = validate[validate.user_nickname==user_dict[userid]].town.unique().tolist()
+        for enum, recomendation in enumerate(recs):
+            if enum == len(gt): break  
+            if town_dict[recomendation[0]] in gt:
+                tp += 1.
+            elif town_dict[recomendation[0]] not in gt:
+                fp += 1.
+
+        for real in gt:
+            if town_dict_inverse[real] not in [r[0] for r in recs]:
+                fn += 1.
+    print('tp:{}, fp:{}, fn:{}'.format(tp,fp,fn))
+    precisionN = tp/(tp+fp)
+    recallN = tp/(tp+fn)
+    return precisionN, recallN
+
+
+def rmse_implicit(user_dict, town_dict_inverse, model, user_item_df, validate, item_users):
+    rmse_pred, rmse_gt = [],[]
+    for userid in list(user_dict.keys())[:3000]:
+        recs = model.recommend(userid, item_users, N=len(user_item_df.columns.tolist()))
+        gt = validate[validate.user_nickname==user_dict[userid]]#.town.unique().tolist()
+        if len(gt)==0: continue
+        for ind, row in gt.iterrows():
+            try:
+                pred_val = recs[[r[0] for r in recs].index(town_dict_inverse[row['town']])][1]
+            except:
+                print(ValueError, row['town'])
+                continue
+            rmse_gt.append(row['count'])
+            rmse_pred.append(pred_val)
+    rmse = math.sqrt(mean_squared_error(rmse_gt, rmse_pred))
+    return rmse
+
 
 # Class
 
@@ -103,28 +207,37 @@ class locationRec(object):
         self.recall_test = None
 
         self.model = None
+        self.preproccesing = None
 
 
     def datapipeline(self, preproccesing=1):
+        self.preproccesing = preproccesing
         if preproccesing==1:
             self.user_item_df = pd.read_pickle('User_Item.pckl')
             self.train = pd.read_pickle('train.pckl')
             self.validate = pd.read_pickle('validate.pckl')
             self.test = pd.read_pickle('test.pckl')
         if preproccesing==2:
-            preprocess_binary()
+            if os.path.isfile('User_Item2.pckl') and os.path.isfile('train2.pckl') and os.path.isfile('validate2.pckl') and os.path.isfile('test2.pckl'):
+                pass
+            else:
+                preprocess_binary()
             self.user_item_df = pd.read_pickle('User_Item2.pckl')
             self.train = pd.read_pickle('train2.pckl')
             self.validate = pd.read_pickle('validate2.pckl')
             self.test = pd.read_pickle('test2.pckl')
 
 
-    def train_model(self, model_type='SVD', latent_dimension=50):
+    def train_model(self, model_type='SVD', latent_dimension=50, N=10):
         if model_type == 'SVD':
             print('Training using SVD model...')
-            self.preds = svd_model(self.user_item_df, latent_dimension)
-        elif model_type == 'SVD++':
-            pass
+            self.preds = svd_model(self.user_item_df, latent_dimension, N)
+        elif model_type == 'SVD_implicit':
+            print('Training using SVD_implicit model...')
+            #self.preds = implicit_model(self.user_item_df, self.train, self.validate, latent_dimension, N, self.preproccesing)
+            self.model, self.precision_train, self.recall_train, self.precision_val, self.recall_val, self.rmse_train, self.rmse_val = \
+                                implicit_model(self.user_item_df, self.train, self.validate, latent_dimension, N, self.preproccesing)
+            #TODO compute self.preds using self.model
         elif model_type == 'AutoEncoder':
             pass
         print('Done')
@@ -134,9 +247,9 @@ class locationRec(object):
         if data == 'val':
             self.rmse_val = compute_rmse(self.preds, self.validate)
         elif data == 'train':
-            self.rmse_val = compute_rmse(self.preds, self.train)
+            self.rmse_train = compute_rmse(self.preds, self.train)
         elif data == 'test':
-            self.rmse_val = compute_rmse(self.preds, self.test)
+            self.rmse_test = compute_rmse(self.preds, self.test)
 
 
     def eval_precision_N(self, N, data = 'val'):
